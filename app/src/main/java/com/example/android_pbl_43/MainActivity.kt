220aa609904201273f3090c03d03b5ad213605ec
LeeMyeongSeong <goodthinglee@gmail.com>
2022-11-25 18:28:53 +0900

    상세화면 페이지, 컨텐츠 모델

--- app/src/main/java/com/example/android_pbl_43/MainActivity.kt
+++ app/src/main/java/com/example/android_pbl_43/MainActivity.kt
@@ -5,48 +5,77 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.android_pbl_43.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                detailViewFragment.arguments = bundle
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alaramFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alaramFragment).commit()
+                true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment)
+                    .commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1
+        )
 
-        var bottom_navigation: BottomNavigationView= findViewById(R.id.bottom_navigation)
-        bottom_navigation.setOnNavigationItemSelectedListener { it ->
-            when(it.itemId) {
-                R.id.action_home -> {
-                    var detailViewFragment = DetailViewFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
-                    true
-                }
-                R.id.action_search -> {
-                    var gridFragment = GridFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
-                    true
-                }
-                R.id.action_add_photo -> {
-                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        startActivity(Intent(this, AddPhotoActivity::class.java))
-                    }
-                    true
-                }
-                R.id.action_favorite_alarm -> {
-                    var alaramFragment = AlarmFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.main_content, alaramFragment).commit()
-                    true
-                }
-                R.id.action_account -> {
-                    var userFragment = UserFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
-                    true
-                }
-                else -> false
-            }
-        }
+        //Set default screen
+        bottom_navigation.selectedItemId = R.id.action_home
+        //registerPushToken()
     }
 }
--- app/src/main/java/com/example/android_pbl_43/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/android_pbl_43/navigation/AddPhotoActivity.kt
@@ -9,20 +9,30 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import com.example.android_pbl_43.R
+import com.example.android_pbl_43.navigation.model.ContentDTO
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import kotlinx.android.synthetic.main.activity_add_photo.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     lateinit var storage: FirebaseStorage
-    var photoUri : Uri? = null
+    var photoUri: Uri? = null
+    var firestore: FirebaseFirestore? = null
+    var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         // 앨범 열기
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -38,12 +48,12 @@ class AddPhotoActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == PICK_IMAGE_FROM_ALBUM) {
-            if(resultCode == Activity.RESULT_OK) {
+        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
+            if (resultCode == Activity.RESULT_OK) {
                 photoUri = data?.data
                 var addphoto_image = findViewById<ImageView>(R.id.addphoto_image)
                 addphoto_image.setImageURI(photoUri)
-            }else {
+            } else {
                 finish()
             }
         }
@@ -56,8 +66,34 @@ class AddPhotoActivity : AppCompatActivity() {
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
         // 이미지 업로드
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            //Insert download of image //﻿다운로드 URL를 컨텐츠 DTO에 넣어주고
+            contentDTO.imageUrl = uri.toString()
+
+            //Insert uid of user //﻿유저 아이디를 uid에 넣고
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //Insert userId //﻿유저 아이디에 이메일을 넣어준다.
+            contentDTO.userId = auth?.currentUser?.email
+
+            //Insert explain of content //사용자가 입력한 설명글을 넣어준다.
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            //Insert timestamp //작성한 시간을 넣어준다.
+            contentDTO.timestamp = System.currentTimeMillis().toString()
+
+            //컨텐츠 DTO를 images 안에 넣어줄것입니다.
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            //정상적으로 종료되었다는 프래그값을 넘겨주기 위함.
+            setResult(Activity.RESULT_OK)
+
+            //finish //종료하고 창을 닫아줍니다.
+            finish()
         }
     }
 }
