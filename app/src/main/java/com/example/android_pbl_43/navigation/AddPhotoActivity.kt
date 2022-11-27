be7be70bf35a2324265d5d2122197a8a3472c80b
dpdnjs512 <95975086+dpdnjs512@users.noreply.github.com>
2022-11-27 12:31:43 +0900

    Add files via upload

--- app/src/main/java/com/example/android_pbl_43/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/android_pbl_43/navigation/AddPhotoActivity.kt
@@ -2,12 +2,17 @@ package com.example.android_pbl_43.navigation
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.android_pbl_43.R
 import com.example.android_pbl_43.navigation.model.ContentDTO
 import com.google.android.gms.tasks.Task
@@ -15,32 +20,40 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
-import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.net.URL
+import java.security.Permissions
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlinx.android.synthetic.main.activity_add_photo.*
 
 class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
-    lateinit var storage: FirebaseStorage
+    var storage: FirebaseStorage? = null
     var photoUri: Uri? = null
-    var firestore: FirebaseFirestore? = null
-    var auth: FirebaseAuth? = null
+
+   var firestore: FirebaseFirestore? = null
+   var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
         storage = FirebaseStorage.getInstance()
-        auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
 
         // 앨범 열기
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
         startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
 
+        //addphoto_image.setOnClickListener {
+        //    val photoPickerIntent = Intent(Intent.ACTION_PICK)
+        //    photoPickerIntent.type = "image/*"
+        //   startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+        //}
+
         // 이미지 업로드 이벤트
-        var addphoto_btn_upload = findViewById<Button>(R.id.addphoto_btn_upload)
         addphoto_btn_upload.setOnClickListener {
             contentUpload()
         }
@@ -49,10 +62,9 @@ class AddPhotoActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_FROM_ALBUM) {
-            if (resultCode == Activity.RESULT_OK) {
-                photoUri = data?.data
-                var addphoto_image = findViewById<ImageView>(R.id.addphoto_image)
-                addphoto_image.setImageURI(photoUri)
+            if (resultCode == Activity.RESULT_OK) { //사진 선택 시
+                photoUri = data?.data //이미지 경로
+                addphoto_image.setImageURI(photoUri) //이미지뷰에 표시
             } else {
                 finish()
             }
@@ -60,40 +72,36 @@ class AddPhotoActivity : AppCompatActivity() {
     }
 
     fun contentUpload() {
-        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        var imageFileName = "IMAGE_" + timestamp + "_.png"
-
-        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) //파일 이름
+        val imageFileName = "IMAGE_" + timeStamp + "_.png"
+        val storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
         // 이미지 업로드
-        storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
-            return@continueWithTask storageRef.downloadUrl
-        }?.addOnSuccessListener { uri ->
-            var contentDTO = ContentDTO()
+        storageRef?.putFile(photoUri!!)
+            ?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl}
+        ?.addOnSuccessListener { uri ->
+                var contentDTO = ContentDTO()
+                contentDTO.imageUrl = uri!!.toString()
+                contentDTO.uid = auth?.currentUser?.uid
+                contentDTO.explain = addphoto_edit_explain.text.toString()
+                contentDTO.userId = auth?.currentUser?.email
+                contentDTO.timestamp = System.currentTimeMillis()
+                firestore?.collection("images")?.document()?.set(contentDTO)
 
-            //Insert download of image //﻿다운로드 URL를 컨텐츠 DTO에 넣어주고
-            contentDTO.imageUrl = uri.toString()
+                setResult(Activity.RESULT_OK)
 
-            //Insert uid of user //﻿유저 아이디를 uid에 넣고
-            contentDTO.uid = auth?.currentUser?.uid
-
-            //Insert userId //﻿유저 아이디에 이메일을 넣어준다.
-            contentDTO.userId = auth?.currentUser?.email
-
-            //Insert explain of content //사용자가 입력한 설명글을 넣어준다.
-            contentDTO.explain = addphoto_edit_explain.text.toString()
-
-            //Insert timestamp //작성한 시간을 넣어준다.
-            contentDTO.timestamp = System.currentTimeMillis().toString()
-
-            //컨텐츠 DTO를 images 안에 넣어줄것입니다.
-            firestore?.collection("images")?.document()?.set(contentDTO)
-
-            //정상적으로 종료되었다는 프래그값을 넘겨주기 위함.
-            setResult(Activity.RESULT_OK)
-
-            //finish //종료하고 창을 닫아줍니다.
-            finish()
+                finish()
+            }
         }
-    }
+
+
+
+
+       // }?.addOnFailureListener {
+            //Toast.makeText(this, getString(R.string.upload_fail)
+            //    , Toast.LENGTH_SHORT).show()
+       // }
+
+
 }
--- app/src/main/java/com/example/android_pbl_43/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/android_pbl_43/navigation/DetailViewFragment.kt
@@ -1,89 +1,209 @@
 package com.example.android_pbl_43.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.android_pbl_43.MainActivity
 import com.example.android_pbl_43.R
+import com.example.android_pbl_43.databinding.ItemDetailBinding
+import com.example.android_pbl_43.databinding.FragmentDetailBinding
 import com.example.android_pbl_43.navigation.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider
+import com.google.firebase.firestore.ktx.toObject
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
+
 class DetailViewFragment : Fragment() {
-    var firestore : FirebaseFirestore? = null
-    var uid : String? = null
+    var firestore: FirebaseFirestore? = null
+    var uid: String?= null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
-        firestore = FirebaseFirestore.getInstance()
         uid = FirebaseAuth.getInstance().currentUser?.uid
+        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+        firestore = FirebaseFirestore.getInstance()
 
-        view.detailviewfragment_recyclerview.adapter = DetailVewReecyclerViewAdater()
-        view.detailviewfragment_recyclerview.layoutManager =  LinearLayoutManager(activity)
+        view.detailviewfragment_recyclerview.adapter = DetailRecyclerViewAdapter()
+        view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
 
         return view
     }
-    inner class DetailVewReecyclerViewAdater : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
-        var contentUidList : ArrayList<String> = arrayListOf()
 
-        init{
-            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                contentDTOs.clear()
-                contentUidList.clear()
-                //Sometimes, This code return null of querySnapshot when it signout
-                if(querySnapshot == null) return@addSnapshotListener
+    inner class DetailRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-                for(snapshot in querySnapshot!!.documents){
-                    var item = snapshot.toObject(ContentDTO::class.java)
-                    contentDTOs.add(item!!)
-                    contentUidList.add(snapshot.id)
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList: ArrayList<String> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp")
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    contentDTOs.clear()
+                    contentUidList.clear()
+                    for (snapshot in querySnapshot!!.documents) {
+                        val item = snapshot.toObject(ContentDTO::class.java)
+                        contentDTOs.add(item!!)
+                        contentUidList.add(snapshot.id)
+                    }
+                    notifyDataSetChanged()
                 }
-                notifyDataSetChanged()
-            }
         }
 
-        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail,p0,false)
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
             return CustomViewHolder(view)
         }
+
         inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
         override fun getItemCount(): Int {
             return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val viewHolder = (holder as CustomViewHolder).itemView
+
+            //유저 아이디
+            viewHolder.detailviewitem_profile_textview.text =
+                contentDTOs!![position].userId
+
+            //이미지
+            Glide.with(holder.itemView.context).load(
+                contentDTOs!![position].imageUrl)
+                .into(viewHolder.detailviewitem_imageview_content)
+
+            //설명 텍스트
+            viewHolder.detailviewitem_explain_textview.text =
+                contentDTOs!![position].explain
+
+            //좋아요 카운터 설정
+            viewHolder.detailviewitem_favoritecounter_textview.text = "좋아요" +
+                    contentDTOs!![position].favoriteCount
+
+            //가운데 이미지
+            Glide.with(holder.itemView.context)
+                .load(contentDTOs[position].imageUrl)
+                .into(viewHolder.detailviewitem_profile_image)
+
+            //좋아요 이벤트
+            viewHolder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+            //좋아요 버튼 설정
+            if (contentDTOs!![position].favorites.containsKey(uid)) {
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(
+                    R.drawable.ic_favorite)
+            } else {
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(
+                    R.drawable.ic_favorite_border)
+            }
+
+/*
+        fun getCotents(followers: MutableMap<String, Boolean>?) {
+            imagesSnapshot = firestore?.collection("images")?.orderBy("timestamp")
+               ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                   contentDTOs.clear()
+                   contentUidList.clear()
+                   if (querySnapshot == null) return@addSnapshotListener
+                   for (snapshot in querySnapshot!!.documents) {
+                       var item = snapshot.toObject(ContentDTO::class.java)!!
+                       println(item.uid)
+                        if (followers?.keys?.contains(item.uid))) {
+                           contentDTOs.add(item)
+                           contentUidList.add(snapshot.id)
+                        }
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+
+ */
+
+
+
+
+
+
+
+            firestore?.collection("profileImages")?.document(
+                contentDTOs[position]
+                    .uid!!
+            )?.get()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val url = task.result["image"]
+                    Glide.with(holder.itemView.context)
+                        .load(url)
+                        .apply(
+                            RequestOptions()
+                                .circleCrop()
+                        )
+                        .into(viewHolder.detailviewitem_profile_image)
+                }
+            }
+
+            //UserFragment로 이동
+            viewHolder.detailviewitem_profile_image.setOnClickListener {
+                val fragment = UserFragment()
+                val bundle = Bundle()
+
+                bundle.putString("destinationUid", contentDTOs[position].uid)
+                bundle.putString("userId", contentDTOs[position].userId)
+
+                fragment.arguments = bundle
+                activity!!.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, fragment)
+                    .commit()
+            }
+
+            //comment
+            viewHolder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                startActivity(intent)
+            }
+
 
         }
-        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-            //p0값을 CustomViewHolder 에 캐스팅
-            var viewholder = (p0 as CustomViewHolder).itemView
+        //좋아요 이벤트 기능
+        fun favoriteEvent(position: Int) {
+            val tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+                uid = FirebaseAuth.getInstance().currentUser?.uid
+                val contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
 
-            //UserId
-            viewholder.detailviewitem_profile_textview.text = contentDTOs!![p1].userId
-
-            //Image
-            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_imageview_content)
-
-            //Explan of content
-            viewholder.detailviewitem_explan_textview.text = contentDTOs!![p1].explain
-
-            //likes
-            viewholder.detailviewitem_favoritecount_textview.text = "좋아요 " + contentDTOs!![p1].favoriteCount
-
-            //ProfileImage
-            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_imageview_content)
-
-
+                if (contentDTO!!.favorites.containsKey(uid)) {
+                    contentDTO.favoriteCount = contentDTO.favoriteCount - 1
+                    contentDTO.favorites.remove(uid) //!!추가
+                } else {
+                    contentDTO.favoriteCount = contentDTO.favoriteCount + 1
+                    contentDTO.favorites[uid!!] = true //!!추가
+                }
+                transaction.set(tsDoc, contentDTO)
+            }
         }
     }
 
